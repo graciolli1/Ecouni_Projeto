52ca4fc7e45c6e09500f2149d79a756ca5564400
Leonardo Graciolli <leonardograciolli123@gmail.com>
2024-05-19 23:01:23 -0300

    css create, details, edit

---
+++ Migrations/20240520015800_tabelaColetaAtt2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecouni_Projeto.Migrations
+{
+    /// <inheritdoc />
+    public partial class tabelaColetaAtt2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
