c61a995e6dafa427e9da412b019ff91937c5c160
Kaique Eduardo <zoomkaique1528@gmail.com>
2024-06-18 09:57:26 -0300

    modificação login

--- Controllers/CadastrarsController.cs
+++ Controllers/CadastrarsController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Ecouni_Projeto.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class CadastrarsController : Controller
     {
         private readonly ApplicationDbContext _context;
